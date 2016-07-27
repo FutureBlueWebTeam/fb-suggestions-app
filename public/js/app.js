e1ad04a3f46e6ece0827339f328a1e3de850196f
Thomas Mullen <thomasmullendesign@gmail.com>
2016-07-27 15:57:04 -0400

    sorry, no angular

--- public/js/app.js
+++ public/js/app.js
@@ -1,76 +1,75 @@
 //Data
 var cities = [{
-	city : '8200 Warden Avenue',
-	desc : 'IBM Canada Software Lab - Toronto, 8200 Warden Avenue, Markham, ON L6G 1C7',
-	lat : 43.84886,
-	long : -79.33838
+    city: '8200 Warden Avenue'
+    , desc: 'IBM Canada Software Lab - Toronto, 8200 Warden Avenue, Markham, ON L6G 1C7'
+    , lat: 43.84886
+    , long: -79.33838
 }, {
-	city : '3600 Steeles Avenue East',
-	desc : 'IBM Canada Ltd., 3600 Steeles Avenue East, Markham, ON L3R 9Z7',
-	lat : 43.81838,
-	long : -79.33421
+    city: '3600 Steeles Avenue East'
+    , desc: 'IBM Canada Ltd., 3600 Steeles Avenue East, Markham, ON L3R 9Z7'
+    , lat: 43.81838
+    , long: -79.33421
 }, {
-	city : '120 Bloor Street East',
-	desc : 'IBM, 120 Bloor Street East, Toronto, ON M4W 1B7',
-	lat : 43.67128,
-	long : -79.38389
+    city: '120 Bloor Street East'
+    , desc: 'IBM, 120 Bloor Street East, Toronto, ON M4W 1B7'
+    , lat: 43.67128
+    , long: -79.38389
 }, {
-	city : '3755 Riverside Drive',
-	desc : 'IBM Canada Inc., 3755 Riverside Drive, Ottawa, ON K1V 1B8',
-	lat : 45.34053,
-	long : -75.69045
+    city: '3755 Riverside Drive'
+    , desc: 'IBM Canada Inc., 3755 Riverside Drive, Ottawa, ON K1V 1B8'
+    , lat: 45.34053
+    , long: -75.69045
 }, {
-	city : '770 Palladium Drive',
-	desc : 'IBM, 770 Palladium Drive, Ottawa, ON K2V',
-	lat : 45.29837,
-	long : -75.92174
+    city: '770 Palladium Drive'
+    , desc: 'IBM, 770 Palladium Drive, Ottawa, ON K2V'
+    , lat: 45.29837
+    , long: -75.92174
 }];
 
-//Angular App Module and Controller
-angular.module('fb-suggestions-app', []).controller('MapCtrl', function($scope) {
 
-	var mapOptions = {
-		zoom : 12,
-		center : new google.maps.LatLng(43.84886, -79.33838),
-		mapTypeId : google.maps.MapTypeId.TERRAIN
-	}
+window.onload = function () {
+    var mapOptions = {
+        zoom: 12
+        , center: new google.maps.LatLng(43.84886, -79.33838)
+        , mapTypeId: google.maps.MapTypeId.TERRAIN
+    }
 
-	$scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-	$scope.markers = [];
+    var markers = [];
 
-	var infoWindow = new google.maps.InfoWindow();
+    var infoWindow = new google.maps.InfoWindow();
 
-	var createMarker = function(info) {
+    var createMarker = function (info) {
 
-		var marker = new google.maps.Marker({
-			map : $scope.map,
-			position : new google.maps.LatLng(info.lat, info.long),
-			title : info.city
-		});
-		marker.content = '<div class="infoWindowContent">' + info.desc + '<img class="image-reponsive center center-block" src="http://33xlkmrogb473k7z1dknkdmx.wpengine.netdna-cdn.com/wp-content/uploads/2016/05/sushi7.jpg"/>' + '</div>';
+        var marker = new google.maps.Marker({
+            map: map
+            , position: new google.maps.LatLng(info.lat, info.long)
+            , title: info.city
+        });
+        marker.content = '<div class="infoWindowContent">' + info.desc + '<img class="image-reponsive center center-block" src="http://33xlkmrogb473k7z1dknkdmx.wpengine.netdna-cdn.com/wp-content/uploads/2016/05/sushi7.jpg"/>' + '</div>';
 
-		google.maps.event.addListener(marker, 'click', function() {
-			infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
-			infoWindow.open($scope.map, marker);
-		});
+        google.maps.event.addListener(marker, 'click', function () {
+            infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
+            infoWindow.open(map, marker);
+        });
 
-		$scope.markers.push(marker);
+        markers.push(marker);
 
-	}
-	for ( i = 0; i < cities.length; i++) {
-		createMarker(cities[i]);
-	}
+    }
+    for (i = 0; i < cities.length; i++) {
+        createMarker(cities[i]);
+    }
 
-	$scope.winHeight = function() {
-		return window.innerHeight;
-	};
-	$scope.winWidth = function() {
-		return window.innerWidth ;
-	};
+    var winHeight = function () {
+        return window.innerHeight;
+    };
+    var winWidth = function () {
+        return window.innerWidth;
+    };
 
-	$scope.openInfoWindow = function(e, selectedMarker) {
-		e.preventDefault();
-		google.maps.event.trigger(selectedMarker, 'click');
-	}
-});
+    var openInfoWindow = function (e, selectedMarker) {
+        e.preventDefault();
+        google.maps.event.trigger(selectedMarker, 'click');
+    }
+}
